d5ef32e8cb6f318102bf182d2b713d8f81b79559
enchanter-w <enchanterw@outlook.com>
2025-04-09 18:12:39 +0800

    Never give up

--- src/config.ts
+++ src/config.ts
@@ -15,13 +15,13 @@ export const siteConfig: SiteConfig = {
 		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
+		enable: true,
 		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		position: "bottom", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "So many things to hack , such little time to work.", // Credit text to be displayed
+			url: "https://www.bilibili.com/video/BV1GJ411x7h7/", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
